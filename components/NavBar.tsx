c12eb866517b3b655734898dd24c9b6486172e48
jakemsr <jakemsr@yahoo.com>
2026-06-17 10:14:01 -0700

    fix: Make the link work
    - Add z-index of 100 so NavBar sits above other elements that may have
      absolute placement
    - Make the background transparent and add specific text color
    - Make location dynamic as text instead of an SVG

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function NavBar( {code} : {code: boolean} ) {
+interface NavBarProps {
+  code: boolean;
+  location: string;
+}
+
+export default function NavBar({ code, location }: NavBarProps) {
   return (
-    <div className="flex items-center justify-between w-full h-16 px-4 bg-[#fcfcfc]">
-      <div className="flex">
-        <Link href="/" className="px-4 text-[#1a1b1c] font-semibold text-sm/4 uppercase">
-          Skintrinsic
+    <div className="flex items-center justify-between w-full h-16 px-4 bg-transparent text-[#1a1b1c] z-100">
+      <div className="flex items-center">
+        <Link href="/">
+          <div className="px-4 font-semibold text-sm/4 uppercase">
+            Skintrinsic
+          </div>
         </Link>
-        <img src="/intro.svg" alt="" />
+        <div className="flex items-center justify-left w-41 h-5 opacity-60">
+          <img src="/header-bracket-left.svg" alt="" />
+          <div className="mx-1.5 w-fit h-4 font-semibold text-sm leading-4 tracking-[-2%] uppercase">
+            {location}
+          </div>
+          <img src="/header-bracket-right.svg" alt="" />
+        </div>
       </div>
       <div>
         {code && <img src="/enter-code.svg" alt="" />}
