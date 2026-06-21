a680eae8eb1085667b3bc11d1e0a81fdcb3911e3
jakemsr <jakemsr@yahoo.com>
2026-06-21 11:52:06 -0700

    feat: Add light mode to NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,11 +3,26 @@ import Link from "next/link";
 interface NavBarProps {
   code: boolean;
   location: string;
+  light?: boolean;
 }
 
-export default function NavBar({ code, location }: NavBarProps) {
+export default function NavBar({ code, location, light }: NavBarProps) {
+ 
+   const items = {
+    textColor: "#1a1b1c",
+    leftBracket: "/header-bracket-left.svg",
+    rightBracket: "/header-bracket-right.svg"
+  }
+
+  if (light) {
+    items.textColor = "#fcfcfc";
+    items.leftBracket = "/header-bracket-left-light.svg";
+    items.rightBracket = "/header-bracket-right-light.svg";
+  }
+
+ 
   return (
-    <div className="flex items-center justify-between w-full h-16 px-4 bg-transparent text-[#1a1b1c] z-100">
+    <div className={`flex items-center justify-between w-full h-16 px-4 bg-transparent text-[${items.textColor}] z-100`}>
       <div className="flex items-center">
         <Link href="/">
           <div className="px-4 font-semibold text-sm/4 uppercase">
@@ -15,11 +30,11 @@ export default function NavBar({ code, location }: NavBarProps) {
           </div>
         </Link>
         <div className="flex items-center justify-left w-41 h-5 opacity-60">
-          <img src="/header-bracket-left.svg" alt="" />
+          <img src={items.leftBracket} alt="" />
           <div className="mx-1.5 w-fit h-4 font-semibold text-sm leading-4 tracking-[-2%] uppercase">
             {location}
           </div>
-          <img src="/header-bracket-right.svg" alt="" />
+          <img src={items.rightBracket} alt="" />
         </div>
       </div>
       <div>
